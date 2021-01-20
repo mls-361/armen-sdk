86fcea537f9c5a4f5cb80166024a5dd3cc683fc1
mls-361 <mls-361@protonmail.com>
2021-01-20 16:07:16 +0100

    En cours de développement

--- jw/jw.go
+++ jw/jw.go
@@ -48,28 +48,30 @@ const (
 type (
 	// Job AFAIRE.
 	Job struct {
-		ID             string
-		Name           string
-		Namespace      string
-		Type           string
-		Origin         string
-		Priority       Priority
-		Key            *string
-		Workflow       *string
-		WorkflowFailed *bool
-		Emails         *string
-		Config         datamap.DataMap
-		Data           datamap.DataMap
-		CreatedAt      time.Time
-		Status         Status
-		Error          *string
-		Attempts       int
-		FinishedAt     *time.Time
-		RunAfter       time.Time
-		Result         *string
-		NextStep       *string
-		Weight         int
-		TimeReference  time.Time
+		ID                string
+		Name              string
+		Namespace         string
+		Type              string
+		Origin            string
+		Priority          Priority
+		Key               *string
+		Workflow          *string
+		WorkflowFailed    *bool
+		Emails            *string
+		Config            datamap.DataMap
+		Private           datamap.DataMap
+		Public            datamap.DataMap
+		CreatedAt         time.Time
+		Status            Status
+		Error             *string
+		Attempts          int
+		FinishedAt        *time.Time
+		ExternalReference *string
+		RunAfter          time.Time
+		Result            *string
+		NextStep          *string
+		Weight            int
+		TimeReference     time.Time
 	}
 
 	// Result AFAIRE.
@@ -86,7 +88,7 @@ type (
 )
 
 // NewJob AFAIRE.
-func NewJob(id, name, namespace, _type, origin string, priority Priority, key, emails *string) *Job {
+func NewJob(id, name, namespace, _type, origin string, priority Priority, key *string) *Job {
 	now := time.Now()
 
 	return &Job{
@@ -97,9 +99,6 @@ func NewJob(id, name, namespace, _type, origin string, priority Priority, key, e
 		Origin:        origin,
 		Priority:      priority,
 		Key:           key,
-		Emails:        emails,
-		Config:        make(datamap.DataMap),
-		Data:          make(datamap.DataMap),
 		CreatedAt:     now,
 		Status:        StatusToDo,
 		RunAfter:      now,
@@ -142,12 +141,12 @@ type (
 	// Workflow AFAIRE.
 	Workflow struct {
 		ID                string
-		ExternalReference *string
 		Description       string
 		Origin            string
 		Priority          Priority
 		FirstStep         string
 		AllSteps          map[string]*Step
+		ExternalReference *string
 		Emails            *string
 		Data              datamap.DataMap
 		CreatedAt         time.Time
@@ -156,21 +155,27 @@ type (
 	}
 )
 
+// NewStep AFAIRE.
+func NewStep(namespace, _type string, config, next datamap.DataMap) *Step {
+	return &Step{
+		Namespace: namespace,
+		Type:      _type,
+		Config:    config,
+		Next:      next,
+	}
+}
+
 // NewWorkflow AFAIRE.
-func NewWorkflow(id string, externalRef *string, desc, origin string, priority Priority, fStep string,
-	aSteps map[string]*Step, emails *string) *Workflow {
+func NewWorkflow(id string, desc, origin string, priority Priority, fStep string, aSteps map[string]*Step) *Workflow {
 	return &Workflow{
-		ID:                id,
-		ExternalReference: externalRef,
-		Description:       desc,
-		Origin:            origin,
-		Priority:          priority,
-		FirstStep:         fStep,
-		AllSteps:          aSteps,
-		Emails:            emails,
-		Data:              make(datamap.DataMap),
-		CreatedAt:         time.Now(),
-		Status:            StatusRunning,
+		ID:          id,
+		Description: desc,
+		Origin:      origin,
+		Priority:    priority,
+		FirstStep:   fStep,
+		AllSteps:    aSteps,
+		CreatedAt:   time.Now(),
+		Status:      StatusRunning,
 	}
 }
 
